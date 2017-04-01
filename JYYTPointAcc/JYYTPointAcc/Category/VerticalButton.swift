70fde88c92465f36e3b6386805bdbf33759ba3c6
shjyyt <ytxinguan09@163.com>
2017-04-01 11:58:07 +0800

    fertile Button

--- JYYTPointAcc/JYYTPointAcc/Category/VerticalButton.swift
+++ JYYTPointAcc/JYYTPointAcc/Category/VerticalButton.swift
@@ -20,17 +20,22 @@ class VerticalButton: UIButton {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        imageView?.frame.origin.x=8;
-        imageView?.frame.origin.y=0;
- 
-        imageView?.frame.size.width=frame.size.width-16;
-        imageView?.frame.size.height=(imageView?.frame.size.width)!;
-        
-        titleLabel?.frame.origin.x=0;
-        titleLabel?.frame.origin.y=(imageView?.frame.size.height)! + 8;
-        titleLabel?.frame.size.width=frame.size.width;
-        titleLabel?.frame.size.height=frame.size.height - (titleLabel?.frame.size.width)!;
+        imageView?.center = CGPoint(x: frame.size.width*0.5, y: (imageView?.image?.size.height)!*0.5)
+        titleLabel?.center = CGPoint(x: frame.size.width*0.5, y: frame.size.height - (titleLabel?.frame.size.height)!*0.7 )
+        titleLabel?.textAlignment = .center
+        titleLabel?.sizeToFit()
+//        imageView?.frame = CGRect(x: 8, y: 0, width: (imageView?.image?.size.width)!, height: (imageView?.image?.size.height)!)
+//        titleLabel?.frame = CGRect(x: 0, y: (imageView?.frame.size.height)! + 8, width: frame.size.width, height: frame.size.height - (imageView?.frame.size.height)!)
 
+//        titleLabel?.frame.origin.x=0;
+//        titleLabel?.frame.origin.y=(imageView?.frame.size.height)! + 8;
+//        titleLabel?.frame.size.width=frame.size.width;
+//        titleLabel?.frame.size.height=frame.size.height - (titleLabel?.frame.size.width)!;
+//        imageView?.frame.origin.x=8;
+//        imageView?.frame.origin.y=0;
+//
+//        imageView?.frame.size.width=frame.size.width-16;
+//        imageView?.frame.size.height=(imageView?.frame.size.width)!;
     }
 
 }
